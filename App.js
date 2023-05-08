c1c8295ba9c4f170a047ca6758476d9a597778d7
shaikh88 <shaikhmanik4@gmail.com>
2023-05-08 13:03:34 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Text>Hello world this is a fastlane demo</Text>
+      <Text>Hello world this is a fastlane </Text>
     </View>
   );
 }
